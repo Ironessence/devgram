93dca4d32d7e7d3e9105e5ff278d4350e8ed2e84
Alexandru <alexdnc22@gmail.com>
2023-12-16 11:18:36 +0200

    search

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -67,7 +67,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 mr-5">
         <img
-          src={checkIsLiked(likes, userId) ? 'assets/icons/liked.svg' : 'assets/icons/like.svg'}
+          src={checkIsLiked(likes, userId) ? '/assets/icons/liked.svg' : '/assets/icons/like.svg'}
           alt="like"
           width={20}
           height={20}
@@ -82,7 +82,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
           <Loader />
         ) : (
           <img
-            src={isSaved ? '/assets/icons/saved.svg' : 'assets/icons/save.svg'}
+            src={isSaved ? '/assets/icons/saved.svg' : '/assets/icons/save.svg'}
             alt="like"
             width={20}
             height={20}
