34139095c3e0b4db5cac542769e3f79a689ad8aa
Alexandru <alexdnc22@gmail.com>
2023-12-17 12:54:16 +0200

    re-do imports

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ import {
 } from '@/_root/pages';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
-import SignupForm from '@/_auth/forms/SignupForm';
-import SigninForm from '@/_auth/forms/SigninForm';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
 
 const App = () => {
   return (
